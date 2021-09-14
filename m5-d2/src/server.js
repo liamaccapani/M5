319ea9189e674e7004471e2e3ef9c47495830d70
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-09-14 16:26:14 +0200

    added handler fx in index.js

--- m5-d2/src/server.js
+++ m5-d2/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authorsRouter from "./services/authors/index.js";
+import listEndpoints from "express-list-endpoints";
 
 const server = express()
 const port = 3001
 
 server.use("/authors", authorsRouter)
+console.table(listEndpoints(server))
 
 server.listen(port, () => {
     console.log(`Server running on ${port}`)
--- m5-d2/src/services/authors/index.js
+++ m5-d2/src/services/authors/index.js
@@ -5,18 +5,28 @@ import express from "express";
 const authorsRouter = express.Router()
 
 // Route n.1 -> GET /authors
-authorsRouter.get("/")
+authorsRouter.get("/", (request, response) => {
+    response.send("I am the GET route")
+})
 
 // Route n.1 -> GET /authors/123
-authorsRouter.get("/:authorId")
+authorsRouter.get("/:authorId", (request, response) => {
+    response.send("I am the GET + ID route")
+})
 
 // Route n.1 -> POST /authors
-authorsRouter.post("/")
+authorsRouter.post("/", (request, response) => {
+    response.send("I am the POST route")
+})
 
 // Route n.1 -> PUT /authors/123
-authorsRouter.put("/:authorId")
+authorsRouter.put("/:authorId", (request, response) => {
+    response.send("I am the PUT route")
+})
 
 // Route n.1 -> DELETE /authors/123
-authorsRouter.delete("/:authorId")
+authorsRouter.delete("/:authorId", (request, response) => {
+    response.send("I am the DELETE route")
+})
 
 export default authorsRouter
