38550ca866f230684df4060380e96ae5022dc9c7
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-09-16 09:57:46 +0200

    fixed error handling typo

--- m5-d3/src/errorHandlers.js
+++ m5-d3/src/errorHandlers.js
@@ -3,7 +3,7 @@
 
 export const badRequest = (error, request, response, next) => {
     console.log(`*********${error}`)
-    if(error === 400){
+    if(error.status === 400){
         response.status(400).send()
     } else{
         next(error)
@@ -12,7 +12,7 @@ export const badRequest = (error, request, response, next) => {
 
 export const forbidden = (error, request, response, next) => {
     console.log(error)
-    if(error === 403){
+    if(error.status === 403){
         response.status(403).send()
     } else {
         next(error)
@@ -21,8 +21,8 @@ export const forbidden = (error, request, response, next) => {
 
 export const notFound = (error, request, response, next) => {
     console.log(error)
-    if(error === 404){
-        response.status(404).send({error: err.message})
+    if(error.status === 404){
+        response.status(404).send({error: error.message})
     } else {
         next(error)
     }
