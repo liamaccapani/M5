10502a3c87e39e5bb424ddc99505f5eb15f5322a
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-09-16 08:21:26 +0200

    404 still triggering 500

--- m5-d3/src/errorHandlers.js
+++ m5-d3/src/errorHandlers.js
@@ -1,13 +1,17 @@
+/* no next no chain -> it keeps loading, doesn't reach any endpoint,
+* it doesn't have any way to reach the next error handlers */
+
 export const badRequest = (error, request, response, next) => {
+    console.log(`*********${error}`)
     if(error === 400){
         response.status(400).send()
     } else{
-        // chaining differen errors
         next(error)
     }
 }
 
 export const forbidden = (error, request, response, next) => {
+    console.log(error)
     if(error === 403){
         response.status(403).send()
     } else {
@@ -16,15 +20,16 @@ export const forbidden = (error, request, response, next) => {
 }
 
 export const notFound = (error, request, response, next) => {
+    console.log(error)
     if(error === 404){
-        response.status(404).send(err.message)
+        response.status(404).send({error: err.message})
     } else {
         next(error)
     }
 }
 
 export const serverError = (error, request, response, next) => {
-    console.log(error)
+    console.log(`*********${error}`)
     response.status(500).send("Server generated error")
 }
 
